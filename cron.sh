44b3b319d74e2f96af2339d59bf1609ec1b575ad
popperscomp <81573982+popperscomp@users.noreply.github.com>
2021-03-30 17:30:43 -0400

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,2 +1,1 @@
-rm -rf /dockerCompose/config;sudo git clone https://github.com/popperscomp/seedbox /dockerCompose/config/;sudo git clone https://github.com/popperscomp/seedbox /dockerCompose/config/
 docker-compose -f /dockerCompose/config/compose/deluge.yml -f /dockerCompose/config/compose/jackett.yml up -d
