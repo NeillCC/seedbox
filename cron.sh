10c8af892fc0cbc765d1c90f981c9a6112e4ff40
popperscomp <81573982+popperscomp@users.noreply.github.com>
2021-03-30 19:10:24 -0400

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,1 +1,6 @@
-docker-compose -f /dockerCompose/config/docker-compose/swag.yml -f /dockerCompose/config/docker-compose/deluge.yml -f /dockerCompose/config/docker-compose/jackett.yml -f /dockerCompose/config/docker-compose/graylog.yml -f /dockerCompose/config/docker-compose/heimdall.yml up -d
+docker-compose \
+    -f /dockerCompose/config/docker-compose/swag.yml \
+    -f /dockerCompose/config/docker-compose/deluge.yml \
+    -f /dockerCompose/config/docker-compose/jackett.yml \
+    -f /dockerCompose/config/docker-compose/graylog.yml \
+    -f /dockerCompose/config/docker-compose/heimdall.yml up -d --remove-orphans
